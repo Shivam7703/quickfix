7877f7e554f12ec6b45c8689879c8b8958d650a5
shivam7703 <shivamg7703@gmail.com>
2026-07-29 16:33:45 +0530

    2nd

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -41,7 +41,7 @@ export default async function page({params}: PageProp) {
 
   return (
     <div>
- <AllBanner title={singleproduct?.title} para={singleproduct?.description} slug="products"/>
+ <AllBanner title={singleproduct?.title} para={singleproduct?.description} slug={`products/${singleproduct?.title.slice(0,10)}..`}/>
       <Productdetail pro={singleproduct}/>
       <ProductSection/>
         <ServiceHighlights/>
