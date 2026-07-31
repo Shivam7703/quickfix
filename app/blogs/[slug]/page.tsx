9011a4d6481267934c5f59cb69cd0db6cf00f00c
shivam7703 <shivamg7703@gmail.com>
2026-07-31 16:26:53 +0530

    hmm

---
+++ app/blogs/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import AllBanner from '@/component/global/banner'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+            <AllBanner title='blog-datail' para='Our pinpoint dispensing tech allows for an exact, controlled application. No mess, no runoff—just the need it.' slug="blogs/details"/>
+      
+    </div>
+  )
+}
+
+export default page
